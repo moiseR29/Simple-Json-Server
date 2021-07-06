f8d25cf27003ae9f1be492fc2d3c1d1dfcb06138
Moise Rebatta <moiserebatta@gmail.com>
2021-07-05 21:52:52 -0300

    added middleware cors

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data.json');
+const middlewares = jsonServer.defaults();
 
-const PORT = 3000;
+server.use(middlewares);
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+const PORT = 3001;
 
 server.use(router);
 server.listen(PORT, () =>
